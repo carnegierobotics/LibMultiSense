6ad335dedc7a370ddc64371b2b79a8365b3073e5
psmithcrl <50182943+psmithcrl@users.noreply.github.com>
2021-08-16 22:14:52 -0400

    new camera profile to support full res aux camara, when available (#52)
    * new camera profile to support full res aux camara, when available
    
    * Updated wording for Full_Res_Aux_Cam profile.
    
    * Added deprecation warning to profile
    
    * Changed deprecation warning, removed print to stderr

--- source/LibMultiSense/MultiSenseTypes.hh
+++ source/LibMultiSense/MultiSenseTypes.hh
@@ -168,6 +168,9 @@ static CRL_CONSTEXPR CameraProfile High_Contrast = (1U<<1);
 static CRL_CONSTEXPR CameraProfile Show_ROIs = (1U<<2);
 /** User would like to run spline-based ground surface algorithm on the camera*/
 static CRL_CONSTEXPR CameraProfile Ground_Surface = (1U<<3);
+/** User would like full resolution images from the aux camera regardless of the requested resolution of the stereo pair.
+ *  Warning: This profile will be deprecated in future revisions of the software.*/
+static CRL_CONSTEXPR CameraProfile Full_Res_Aux_Cam = (1U<<4);
 
 /**
  * Class used to request that MultiSense data be sent to a 3rd-party
