8628cd304278cc3c6d829e74c5261f49d2a259e7
magnus <gjerde.magnus@gmail.com>
2022-11-09 16:09:01 -0500

    Added warning if user specifies adapter name under other os than linux

--- source/LibMultiSense/details/channel.cc
+++ source/LibMultiSense/details/channel.cc
@@ -311,7 +311,11 @@ void impl::bind(const std::string &ifName)
                               strerror(errno));
             }
         }
+    #else
+        if (!ifName.empty())
+            CRL_DEBUG("User specified binding to adapter '%s', but this feature is only supported under linux. Ignoring..", ifName.c_str())
     #endif
+
     //
     // Turn non-blocking on.
 #if WIN32
