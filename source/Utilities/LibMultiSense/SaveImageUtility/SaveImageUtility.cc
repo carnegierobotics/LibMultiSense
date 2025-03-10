f386b4a427d09816d4e14e49ab85841e7c79d456
Matt Alvarado <malvarado@carnegierobotics.com>
2025-03-10 14:29:35 -0400

    Properly handle frame query timeouts

--- source/Utilities/LibMultiSense/SaveImageUtility/SaveImageUtility.cc
+++ source/Utilities/LibMultiSense/SaveImageUtility/SaveImageUtility.cc
@@ -183,7 +183,7 @@ int main(int argc, char** argv)
     if (const auto status = channel->set_configuration(config); status != lms::Status::OK)
     {
         std::cerr << "Cannot set config" << std::endl;
-        return 1;
+        //return 1;
     }
 
     std::vector<lms::DataSource> image_streams{};
@@ -221,8 +221,9 @@ int main(int argc, char** argv)
                 {
                     save_image(image_frame.value(), stream);
                 }
+
+                ++saved_images;
             }
-            ++saved_images;
         }
 
         if (const auto status = channel->get_system_status(); status)
