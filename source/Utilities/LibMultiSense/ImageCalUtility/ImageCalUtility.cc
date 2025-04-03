1f71c599dcfe8aa6d161d119cd2776c2941f2ecb
Matt Alvarado <malvarado@carnegierobotics.com>
2025-04-03 12:00:16 -0400

    Fix a typo in the image cal utility

--- source/Utilities/LibMultiSense/ImageCalUtility/ImageCalUtility.cc
+++ source/Utilities/LibMultiSense/ImageCalUtility/ImageCalUtility.cc
@@ -122,7 +122,7 @@ void write_cal(std::ofstream &intrinsics, std::ofstream &extrinsics, const lms::
 {
     writeMatrix(intrinsics, "M" + std::to_string(index), 3, 3, &cal.K[0][0]);
     writeMatrix(intrinsics, "D" + std::to_string(index), 1, cal.D.size(), cal.D.data());
-    writeMatrix(extrinsics, "R" + std::to_string(index), 3, 3, &cal.K[0][0]);
+    writeMatrix(extrinsics, "R" + std::to_string(index), 3, 3, &cal.R[0][0]);
     writeMatrix(extrinsics, "P" + std::to_string(index), 3, 4, &cal.P[0][0]);
 }
 
