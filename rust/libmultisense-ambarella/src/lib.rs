a67aef62435fc9854582a560c9fb6154951b555e
Woodrow Douglass <wdouglass@carnegierobotics.com>
2025-05-06 17:30:08 -0400

    Add webrtc, manage a peer connection with the mswebrtcimpl object

--- rust/libmultisense-ambarella/src/lib.rs
+++ rust/libmultisense-ambarella/src/lib.rs
@@ -1,14 +1,26 @@
+use std::sync::Arc;
+use webrtc::peer_connection::RTCPeerConnection;
+use wrtc::new_peer_connection;
+
 pub struct MswebrtcImpl {
-    pub addr: u32,
+    rt: tokio::runtime::Runtime,
+    pc: Arc<RTCPeerConnection>,
 }
 
 #[no_mangle]
 pub extern "C" fn create_mswebrtc_impl() -> *mut MswebrtcImpl {
-    let out = Box::new(MswebrtcImpl { addr: 0 });
+    let rt = tokio::runtime::Runtime::new().expect("Failed to create async runtime");
+    let peer_connection = rt
+        .block_on(new_peer_connection())
+        .expect("Failed to create wrtc peer connection");
+    let out = Box::new(MswebrtcImpl {
+        rt: rt,
+        pc: peer_connection,
+    });
     Box::into_raw(out)
 }
 
 #[no_mangle]
 pub extern "C" fn destroy_mswebrtc_impl(obj: *mut MswebrtcImpl) {
-    unsafe { drop(Box::from_raw(obj)) };
+    drop(unsafe { Box::from_raw(obj) });
 }
