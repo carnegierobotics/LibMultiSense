3e59061209ca4ce39dc94a861c7ff558df558786
Woodrow Douglass <wdouglass@carnegierobotics.com>
2025-06-10 09:26:50 -0400

    transition this library from webrtc-rs to str0m

--- rust/libmultisense-ambarella/src/lib.rs
+++ rust/libmultisense-ambarella/src/lib.rs
@@ -1,73 +1,121 @@
-use std::sync::Arc;
-use webrtc::{
-    api::media_engine::MediaEngine,
-    api::APIBuilder,
-    interceptor::registry::Registry,
-    peer_connection::{
-        configuration::RTCConfiguration, sdp::session_description::RTCSessionDescription,
-        RTCPeerConnection,
-    },
-};
+use std::net::UdpSocket;
+use str0m::{change::SdpOffer, Candidate, Rtc};
+use systemstat::{Platform, System};
 
 pub struct MswebrtcImpl {
-    rt: tokio::runtime::Runtime,
-    pc: Arc<RTCPeerConnection>,
+    pc: Rtc,
+    sockets: Vec<UdpSocket>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct MswebrtcError {
+    what: String,
+    err: Option<Box<dyn std::error::Error>>,
 }
 
 impl MswebrtcImpl {
-    fn connect(&self, host: String) -> bool {
+    fn connect_internal(&mut self, host: String) -> Result<(), MswebrtcError> {
         let client = reqwest::blocking::Client::builder()
             .danger_accept_invalid_certs(true)
             .build()
             .unwrap();
-        let offer = match client.get(host.clone() + "/offer").send() {
-            Ok(response) => {
-                let text = response.text().unwrap();
-                Some(text)
-            }
-            Err(e) => None,
-        }
-        .expect("Failed to get offer");
+        let offer = SdpOffer::from_sdp_string(
+            client
+                .get(host.clone() + "/offer")
+                .send()
+                .map_err(|e| MswebrtcError {
+                    what: "Failed to request offer".to_owned(),
+                    err: Some(Box::new(e)),
+                })?
+                .text()
+                .map_err(|e| MswebrtcError {
+                    what: "Failed to extract offer from HTTP response".to_owned(),
+                    err: Some(Box::new(e)),
+                })?
+                .as_str(),
+        )
+        .map_err(|e| MswebrtcError {
+            what: "Failed to parse offer".to_owned(),
+            err: Some(Box::new(e)),
+        })?;
 
-        let answer = self.rt.block_on(async {
-            match RTCSessionDescription::offer(offer) {
-                Ok(offer) => {
-                    // set the remote description
-                    //let asdf = offer;
-                    //asdf.sdp;
-                    //dbg!(&offer);
-                    self.pc.set_remote_description(offer).await.unwrap();
+        //enumerate local ip addresses to enumerate ice candidates
+        {
+            let system = System::new();
+            let networks = system.networks().map_err(|e| MswebrtcError {
+                what: "Failed to enumarate networks".to_owned(),
+                err: Some(Box::new(e)),
+            })?;
 
-                    // Create an answer
-                    let answer = self.pc.create_answer(None).await.unwrap();
-
-                    // Sets the LocalDescription, and starts our UDP listeners
-                    self.pc.set_local_description(answer.clone()).await.unwrap();
-
-                    // Create channel that is blocked until ICE Gathering is complete
-                    let mut gather_complete = self.pc.gathering_complete_promise().await;
-
-                    let _ = gather_complete.recv().await;
-                    //Ok(answer)
-
-                    if let Some(local_desc) = self.pc.local_description().await {
-                        Ok(local_desc)
-                    } else {
-                        println!("failed to generate local description");
-                        Err(String::from("failed to generate local description"))
+            let mut found = false;
+            for net in networks.values() {
+                for n in &net.addrs {
+                    if let systemstat::IpAddr::V4(v) = n.addr {
+                        if !v.is_loopback() && !v.is_link_local() && !v.is_broadcast() {
+                            let socket =
+                                UdpSocket::bind(format!("{v}:0")).map_err(|e| MswebrtcError {
+                                    what: "Failed to bind socket".to_owned(),
+                                    err: Some(Box::new(e)),
+                                })?;
+                            let candidate = Candidate::host(
+                                socket.local_addr().map_err(|e| MswebrtcError {
+                                    what: "failed to get address for socket".to_owned(),
+                                    err: Some(Box::new(e)),
+                                })?,
+                                "udp",
+                            )
+                            .map_err(|e| MswebrtcError {
+                                what: "Failed to create ICE candidate".to_owned(),
+                                err: Some(Box::new(e)),
+                            })?;
+                            if self.pc.add_local_candidate(candidate).is_some() {
+                                found = true;
+                                self.sockets.push(socket);
+                            }
+                        }
                     }
                 }
-                Err(e) => Err(String::from("failed to parse offer")),
             }
-        });
+            if !found {
+                Err(MswebrtcError {
+		    what: "Failed to find valid local ICE candidate. Do we have any network interfaces?".to_owned(),
+		    err: None
+		})?
+            }
+        }
+        let answer = self
+            .pc
+            .sdp_api()
+            .accept_offer(offer)
+            .map_err(|e| MswebrtcError {
+                what: "Failed to accept RTC offer".to_owned(),
+                err: Some(Box::new(e)),
+            })?;
 
-        let answer = serde_json::to_string(&answer).expect("Failed to serialize answer");
+        let answer = serde_json::to_string(&answer).map_err(|e| MswebrtcError {
+            what: "Failed to serialize answer".to_owned(),
+            err: Some(Box::new(e)),
+        })?;
         client
             .post(host + "/answer")
             .header("content-type", "application/json")
             .body(answer)
             .send()
-            .is_ok()
+            .map(|_| Ok(()))
+            .map_err(|e| MswebrtcError {
+                what: "Failed to post answer to server".to_owned(),
+                err: Some(Box::new(e)),
+            })?
+    }
+    fn connect(&mut self, host: String) -> bool {
+        match self.connect_internal(host) {
+            Ok(_) => true,
+            Err(e) => {
+                println!("ERROR CONNECTING: {:?}", e);
+                false
+            }
+        }
     }
     fn disconnect(&self) {
         println!("DISCONNECT");
@@ -76,19 +124,11 @@ impl MswebrtcImpl {
 
 #[no_mangle]
 pub extern "C" fn create_mswebrtc_impl() -> *mut MswebrtcImpl {
-    let rt = tokio::runtime::Runtime::new().expect("Failed to create async runtime");
-    let registry = Registry::new();
-    let m = MediaEngine::default();
-    let api = APIBuilder::new()
-        .with_media_engine(m)
-        .with_interceptor_registry(registry)
-        .build();
-    let config = RTCConfiguration::default();
-    let pc = Arc::new(
-        rt.block_on(api.new_peer_connection(config))
-            .expect("failed to make new peer connection"),
-    );
-    let out = Box::new(MswebrtcImpl { rt: rt, pc: pc });
+    let pc = Rtc::new();
+    let out = Box::new(MswebrtcImpl {
+        pc: pc,
+        sockets: Vec::new(),
+    });
     Box::into_raw(out)
 }
 
@@ -99,7 +139,7 @@ pub extern "C" fn connect_mswebrtc_impl(
 ) -> bool {
     let (r, addr) = unsafe {
         (
-            &*obj,
+            &mut *obj,
             std::ffi::CStr::from_ptr(addr as *const i8)
                 .to_str()
                 .unwrap()
