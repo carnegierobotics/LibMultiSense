1aab824908cdd8b540e0e66476b584b86ce475df
Matt Alvarado <malvarado@carnegierobotics.com>
2026-06-02 14:10:43 -0400

    Bump version to 7.9 (#303)

--- source/Legacy/include/MultiSense/details/channel.hh
+++ source/Legacy/include/MultiSense/details/channel.hh
@@ -283,7 +283,7 @@ private:
     //
     // The version of this API
 
-    static CRL_CONSTEXPR VersionType API_VERSION = 0x0708; // 7.8
+    static CRL_CONSTEXPR VersionType API_VERSION = 0x0709; // 7.9
 
     //
     // Misc. internal constants
